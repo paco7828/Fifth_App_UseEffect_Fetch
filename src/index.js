68d37e755854f82b07589f950193c977f4ae3064
paco7828 <koszegipatrik888@gmail.com>
2024-02-13 19:48:42 +0100

    Final commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Buttons({ content, onClick }) {
+  return <button onClick={() => onClick(content)}>{content}</button>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Comp() {
+  const [reqQuery, setReqQuery] = useState("");
+  const [response, setResponse] = useState("");
+  const [queryId, setQueryId] = useState("");
+  const buttonContents = ["users", "posts", "albums"];
+
+  const initQuery = (content) => {
+    setReqQuery(content);
+  }
+
+  const handleChange = (e) => {
+    setQueryId(e.target.value);
+  }
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${reqQuery + (queryId === "" ? "" : `?id=${queryId}`)}`)
+      .then(res => res.json())
+      .then(data => {
+        JSON.stringify(data) === "[]" || JSON.stringify(data) === "{}" ? setResponse("No available requested data") : setResponse(data);
+      })
+      .catch(err => setResponse(err));
+  }, [reqQuery, queryId]);
+
+  return (
+    <div>
+      <h1>Choose what to request:</h1>
+      {buttonContents.map((btnContent, index) => <Buttons key={index} content={btnContent} onClick={initQuery} />)}
+      <br />
+      <input type="number" placeholder="ID... (optional)" value={queryId} onChange={handleChange} />
+      <h2>Requested data:</h2>
+      <p>{JSON.stringify(response)}</p>
+    </div>
+  );
+
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Comp />);
